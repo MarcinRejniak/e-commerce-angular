0d2b471243cf7c00575281ab35a0d95445dbb2cd
MarcinRejniak <marcinrejniak02@gmail.com>
2026-03-20 17:18:42 +0100

    update product-list component to subscribe to data from Angular service

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal, OnInit} from '@angular/core';
+import {Product} from '../../common/product';
+import {ProductService} from '../../services/product.service';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {}
+export class ProductList implements OnInit {
+
+  private readonly productService = inject(ProductService);
+
+  products =  signal<Product[]>([]);
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products.set(data)
+      });
+  }
+}
