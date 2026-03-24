071757695f734afbeb2b8b21f35dc9036b90347d
MarcinRejniak <marcinrejniak02@gmail.com>
2026-03-24 10:45:01 +0100

    add class product-category and component product-category-manu

---
+++ src/app/components/product-category-menu/product-category-menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-category-menu',
+  imports: [],
+  templateUrl: './product-category-menu.html',
+  styleUrl: './product-category-menu.css',
+})
+export class ProductCategoryMenu {}
