2172e79b21cf4d6141c498b2e0952b458184850d
MarcinRejniak <marcinrejniak02@gmail.com>
2026-03-27 19:27:52 +0100

    implement checkout form layout

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -1,9 +1,11 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {CartService} from '../../services/cart.service';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CurrencyPipe],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
@@ -12,6 +14,7 @@ export class Checkout implements OnInit{
   checkoutFormGroup!: FormGroup;
 
   private readonly formBuilder = inject(FormBuilder);
+  public readonly cartService = inject(CartService);
 
   ngOnInit(): void {
 
@@ -20,11 +23,47 @@ export class Checkout implements OnInit{
         firstName: [''],
         lastName: [''],
         email: [''],
+      }),
+      shippingAddress: this.formBuilder.group({
+        country: [''],
+        street: [''],
+        city: [''],
+        state: [''],
+        zipcode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        country: [''],
+        street: [''],
+        city: [''],
+        state: [''],
+        zipcode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
       })
     })
   }
 
   onSubmit() {
 
+  }
+
+  copyShippingAddressToBillingAddress(event: Event) {
+
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    if (isChecked) {
+
+      const shippingAddress = this.checkoutFormGroup.get('shippingAddress')?.value;
+
+      this.checkoutFormGroup.get('billingAddress')?.setValue(shippingAddress);
+    } else {
+      this.checkoutFormGroup.get('billingAddress')?.reset();
+    }
   }
 }
